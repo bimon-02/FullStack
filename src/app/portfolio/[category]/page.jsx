5d70ea20f77b892a7184814e08ef7b2805e04366
bimon-02 <142576126+bimon-02@users.noreply.github.com>
2023-08-19 18:34:20 +0000

    Pending changes exported from your codespace

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,30 @@
 import react from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/button";
 
-const Category = () => {
+const Category = ({ params }) => {
+  console.log(params);
   return (
-    <div>
-      <h1>Category</h1>
+    <div className={styles.container}>
+      <h1 className={styles.categoryTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Digital Storyteller</h1>
+          <p className={styles.description}>
+            Handcrafting award winning digital experiences
+          </p>
+          <Button url="#" text="See More" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://www.pexels.com/photo/sea-black-and-white-dawn-landscape-17118488/"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
     </div>
   );
 };
