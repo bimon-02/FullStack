ae27ad3b5b300257692474062e93ff84881494a3
bimon-02 <142576126+bimon-02@users.noreply.github.com>
2023-08-21 04:23:50 +0000

    Pending changes exported from your codespace

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,30 @@
 import react from "react";
 import styles from "./page.module.css";
-
+import Button from "@/components/button/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/7630928/pexels-photo-7630928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            // fill={true}
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test </h1>
+          <p className={styles.description}>descrip </p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
